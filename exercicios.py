58f5d68218ad84cae875bcf8a01a934f98d8dff4
Caio Souza <caiohas.2908@gmail.com>
2025-01-13 18:06:15 -0300

    adicionando o primeiro exercicio realizado

---
+++ exercicios.py
@@ -0,0 +1,13 @@
+# Inteiros (int)
+
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+num_01 = int(input("Inserir um numero inteiro: "))
+num_02 = int(input("Inserir outro numero inteiro: "))
+
+resultado = num_01 // num_02
+print(resultado)
+
+# 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
